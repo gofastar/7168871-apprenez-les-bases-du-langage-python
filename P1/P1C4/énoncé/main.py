e7f0e0ae0e277c412cc531233c711bab181f1ae8
gofastar <relouboy.1@gmail.com>
2025-07-01 10:53:10 +0200

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,8 @@
-# Écrivez votre code ici !
+age = 25 
+prenom = "Jean"
+
+print(f"je m'appelle {prenom} et j'ai {age} ans.")
+
+age = 35
+
+print(f"Je m'appelle {prenom} et j'ai {age} ans maintenant! ")
