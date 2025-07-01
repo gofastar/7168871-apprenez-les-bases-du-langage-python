613bc4f8e0ce5751547410606bfde7bf3a3cd8bd
gofastar <relouboy.1@gmail.com>
2025-07-01 11:00:23 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,13 @@
-# Écrivez votre code ici !
+age = 25
+Nom = "Jean"
+taille = 1.75
+est_etudiant = True 
+print(age)
+print(Nom)
+print(taille)
+print(est_etudiant)
+
+type(age)
+type(Nom)
+type(taille)
+type(est_etudiant)
